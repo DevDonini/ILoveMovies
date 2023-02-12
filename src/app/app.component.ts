fa4707b904f6be5e87f129a234a005f37a40c721
DevNicolas <nicolasdonini@outlook.com>
2023-02-12 18:05:56 -0300

    ILoveMovies

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import {ThemePalette} from '@angular/material/core';
+import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ILoveMovies';
+  loading = false;
+  color: ThemePalette = 'warn';
+  mode: ProgressSpinnerMode = 'indeterminate';
+  value = 50;
+
+
+  constructor(private router: Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+      } else if (event instanceof NavigationEnd) {
+        setTimeout (() => {
+          this.loading = false;
+       }, 1000);
+      }
+    });
+  }
+
+
+}
